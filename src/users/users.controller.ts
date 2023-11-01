4e4d84dc09696927aa28e7f862c31929c8f11504
Cansu Bozkurt <cansubozkurt@Cansus-MacBook-Pro.local>
2023-11-01 11:40:59 +0300

    user create done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create.user.dto';
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private userService: UsersService) { }
+
+    //create user
+    @Post()
+    async create(@Body() user: CreateUserDto) {
+        return this.userService.create(user);
+    }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/users.entity';
+import { Users } from './entities/users.entity';
 
 @Module({
   imports: [    TypeOrmModule.forFeature([
-    User,
+    Users,
   ]),],
   providers: [UsersService],
   controllers: [UsersController]
